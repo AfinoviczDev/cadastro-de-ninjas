38d32204657b0cc3cedd1a5a0c29c73687c5a378
AfinoviczDev <jpafinovicz@gmail.com>
2026-03-16 15:25:03 -0300

    feat: adicionando missoes repository

---
+++ src/main/java/dev/afinovicz/CadastroDeNinjas/missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.afinovicz.CadastroDeNinjas.missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository <Missoes, Long> {
+}
